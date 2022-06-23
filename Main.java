80df74284d91934e22bb07ff5863c9e11daa9515
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-06-23 20:25:48 +0000

    tekrar eden işlemler

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 class Main {
   public static void main(String[] args) {
     //camelCase
+    //Tekrar eden işlemleri tek kodda değiştir
     
-String internetSubeButonu="İnternet Şubesi";    
+   String internetSubeButonu="İnternet Şubesine gelin";    
+    System.out.println(internetSubeButonu);
+    System.out.println(internetSubeButonu);
+    System.out.println(internetSubeButonu);
+    System.out.println(internetSubeButonu);
+    System.out.println(internetSubeButonu);
     System.out.println(internetSubeButonu);
 
     
